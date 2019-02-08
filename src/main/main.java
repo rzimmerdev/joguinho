4cb3e2f2b91759c3a870c82805d8063eb6990c54
MartinhoS2 <maartins800@gmail.com>
2019-02-08 17:51:21 -0200

    commit

--- src/main/main.java
+++ src/main/main.java
@@ -7,8 +7,7 @@ public class main {
 	int a = 1 + 4;
 =======
 	int a = 2;
-// oi
-//chupa meu sapato
+
 }
 =======
 <<<<<<< HEAD
@@ -18,10 +17,9 @@ oi zimmer
 	
 =======
 	int a = 1 + 2;
->>>>>>> 496e55c994d417574b82fb6905053fad3b912ddc
+
 }
 =======
 	int a = 2;
 }
->>>>>>> 2700759c5d16d8de5c1b2db2be36cf5f5893216e
->>>>>>> d8c67c6a09e57d7fc8b0fcccf1e949e0246abcf3
+
