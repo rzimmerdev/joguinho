1626070d6ce824a32ce5186a93ce161105f3f0f4
gak13Saqf <rzimmerdev@gmail.com>
2019-02-08 18:03:21 -0200

    commit

--- src/main/main.java
+++ src/main/main.java
@@ -1,12 +1,6 @@
 package main;
 
 public class main {
-<<<<<<< HEAD
-  //oi bral eu te mamo
-  //oi Zimmer S2
-	int suamae = 2 + 4;
-=======
-  //oi bral eu te amo apesdar de vc nao conseguir ver isso hj
->>>>>>> f0206c2f053063ac91b05db6dac018df9c841fb9
+
 }
 
